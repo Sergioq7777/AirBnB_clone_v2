f8a8c440787024ebc92cc79160f6e00347c024c2
davidcastellanos <castellanos.johann@gmail.com>
2020-04-02 10:45:59 -0500

    [Update] Fixed pip 8 and condition

--- models/user.py
+++ models/user.py
@@ -15,6 +15,7 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
         email = Column(String(128),
                        nullable=False)
