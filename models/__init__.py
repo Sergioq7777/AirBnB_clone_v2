86fd340eb0c64575e34cdec025abfa8072934b3e
davidcastellanos <castellanos.johann@gmail.com>
2020-04-02 19:54:08 -0500

    [Update] Fixed pep8

--- models/__init__.py
+++ models/__init__.py
@@ -8,7 +8,6 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from os import getenv
 
 
 storage = FileStorage()
--- models/user.py
+++ models/user.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+"""This is the user class"""
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """Representation of a user """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        password = Column(String(128),
-                          nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade="all, delete-orphan")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+class User(BaseModel):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
