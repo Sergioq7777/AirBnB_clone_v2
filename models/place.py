7cdfa1aa498cba91884c4fef8d95c2a9a6459e01
davidcastellanos <castellanos.johann@gmail.com>
2020-04-02 21:25:09 -0500

    [Update] Fixed user.py

--- models/place.py
+++ models/place.py
@@ -1,28 +1,9 @@
 #!/usr/bin/python3
 """This is the place class"""
-import models
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
-from sqlalchemy.orm import relationship
-from models.amenity import Amenity
-from models.review import Review
-from os import getenv
+from models.base_model import BaseModel
 
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id',
-                             String(60),
-                             ForeignKey('places.id'),
-                             nullable=False,
-                             primary_key=True),
-                      Column('amenity_id',
-                             String(60),
-                             ForeignKey('amenities.id'),
-                             nullable=False,
-                             primary_key=True))
-
-
-class Place(BaseModel, Base):
+class Place(BaseModel):
     """This is the class for Place
     Attributes:
         city_id: city id
@@ -37,64 +18,14 @@ class Place(BaseModel, Base):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
-    __tablename__ = 'places'
-    city_id = Column(String(60),
-                     ForeignKey('cities.id'),
-                     nullable=False)
-    user_id = Column(String(60),
-                     ForeignKey('users.id'),
-                     nullable=False)
-    name = Column(String(128),
-                  nullable=False)
-    description = Column(String(1024),
-                         nullable=True)
-    number_rooms = Column(Integer,
-                          nullable=False,
-                          default=0)
-    number_bathrooms = Column(Integer,
-                              nullable=False,
-                              default=0)
-    max_guest = Column(Integer,
-                       nullable=False,
-                       default=0)
-    price_by_night = Column(Integer,
-                            nullable=False,
-                            default=0)
-    latitude = Column(Float,
-                      nullable=True)
-    longitude = Column(Float,
-                       nullable=True)
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = []
-    user = relationship("User", foreign_keys=[user_id])
-    cities = relationship("City", foreign_keys=[city_id])
-    reviews = relationship("Review", cascade="all")
-    amenities = relationship("Amenity",
-                             secondary="place_amenity",
-                             viewonly=False)
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def reviews(self):
-            ''' Returns list of review instances '''
-            results = models.storage.all(Review)
-            review_list = []
-            for review in results.values():
-                if review.place_id == self.id:
-                    review_list.append(review)
-            return review_list
-
-        @property
-        def amenities(self):
-            ''' Returns list of amenity instances '''
-            amenity_list = []
-            results = models.storage.all(Amenity)
-            for amenity in results.values():
-                if amenity.id in self.amenity_ids:
-                    amenity_list.append(amenity)
-            return amenity_list
-
-        @amenities.setter
-        def amenities(self, obj):
-            ''' Appends place id for amenities '''
-            if obj and isinstance(obj, Amenity):
-                type(self).amenity_ids.append(obj.id)
