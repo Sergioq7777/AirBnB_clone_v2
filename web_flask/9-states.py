234fffd86ab04b1a2ec9da1908f5e93d91f77c46
davidcastellanos <castellanos.johann@gmail.com>
2020-04-22 16:04:29 -0500

    [Update] fixed storage

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,25 +1,48 @@
 #!/usr/bin/python3
-"""Flask app to generate html list of all states from storage"""
+'''Flask app to generate html list of all states from storage'''
 from flask import Flask, render_template
 from models import storage
-app = Flask('web_flask')
-app.url_map.strict_slashes = False
 
 
-@app.route('/states/<id>')
-@app.route('/states', defaults={'id': None})
-def specific_state(id):
-    """Render as html alphabetical list of states or specific state entry
-    in `storage` if `id` is a valid identifier"""
-    states = storage.all('State')
-    if id:
-        id = 'State.' + id
-    return render_template('9-states.html', states=states, id=id)
+app = Flask(__name__)
+
+
+def get_state_list():
+    '''gets dict of states from storage'''
+    storage.reload()
+    return [[v.id, v.name] for v in storage.all("State").values()]
+
+
+@app.route('/states', strict_slashes=False)
+def states_list():
+    '''list all states'''
+    states = get_state_list()
+    return render_template('9-states.html', states=states, cities=None)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def cs_by_id(id=None):
+    '''list cities'''
+    states = get_state_list()
+    state = None
+    for x in states:
+        if x[0] == id:
+            state = x[1]
+    if state is None:
+        return render_template('9-states.html', states=None)
+    citiesdict = storage.all("City")
+    cities = []
+    for _, v in citiesdict.items():
+        if v.state_id == id:
+            cities.append([v.id, v.name])
+    return render_template('9-states.html',
+                           states=state,
+                           cities=cities)
 
 
 @app.teardown_appcontext
-def teardown_db(*args, **kwargs):
-    """Close database or file storage"""
+def teardown_db(exception):
+    '''teardown database'''
     storage.close()
 
 
